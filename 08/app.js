534b9c1d40307e00a00a70fc5a618673749f9822
백기훈 <masinogns@gmail.com>
2017-11-03 16:13:35 +0900

    09

--- 08/app.js
+++ 08/app.js
@@ -1,20 +1,20 @@
-// // $('div').css('background-color','black');
-// // $('div').height(200);
-//
-// // <div>요소를 #box에 추가해
-// $('<div>').appendTo( $('#box') )
-//         .text("난 div 요소")
-//         .addClass("border")
-//         .css("background-color","#fff");
-//
-// // $('<div>', {
-// //     text : '난 div 요소',
-// //     addClass : 'border',
-// //     css : {
-// //         "background-color":"#fff"
-// //     }
-// // }).appendTo( $('#box') );
-//
-// $("button").click(function(){
-//     $("<span>Yes!!!</span>").appendTo("p");
-// });
+// $('div').css('background-color','black');
+// $('div').height(200);
+
+// <div>요소를 #box에 추가해
+$('<div>').appendTo( $('#box') )
+        .text("난 div 요소")
+        .addClass("border")
+        .css("background-color","#fff");
+
+// $('<div>', {
+//     text : '난 div 요소',
+//     addClass : 'border',
+//     css : {
+//         "background-color":"#fff"
+//     }
+// }).appendTo( $('#box') );
+
+$("button").click(function(){
+    $("<span>Yes!!!</span>").appendTo("p");
+});
